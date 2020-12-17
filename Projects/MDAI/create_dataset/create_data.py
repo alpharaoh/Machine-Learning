ffc8d95bcd28b6540224f0c00368b2b8ab7f9db1
alpharaoh <alpharaohh@gmail.com>
2020-12-17 15:42:08 +0000

    implemented class - needed for self variables

--- Projects/MDAI/create_dataset/create_data.py
+++ Projects/MDAI/create_dataset/create_data.py
@@ -5,186 +5,228 @@ each foreground object and save the output image and respective boundbox text fi
 in a target folder
 """
 
+#10-13
+
 import cv2
 import os
 import numpy as np
 import random
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from PIL import Image
-#from matplotlib.patches import Rectangle
+from matplotlib.patches import Rectangle
 #import tensorflow as tf
 #import torch
 
 print("Loaded...")
 
-def get_normalized_bbox(x_position: int, y_position: int, width: int, height: int, image_size: tuple):
-   """
-   This function will locate the bounding box of foreground (e.g. Mundo) and then
-   normalise the values from 0-1. This is important step to get YOLO format correct.
+class CreateDataset():
+   def __init__(self, background_path_folder, mundo_path_folder, axe_path_folder, output_path):
+      self.x_mins = []
+      self.y_mins = []
+      self.x_maxes = []
+      self.y_maxes = []
+      self.widths = []
+      self.heights = []
+      self.background_path_folder = background_path_folder
+      self.mundo_path_folder = mundo_path_folder
+      self.axe_path_folder = axe_path_folder
+      self.output_path = output_path
 
-   YOLO doesn't use bounding boxes where xy is the top-left corner, but xy is the 
-   center of the rectangle
-   """
-   # get background screen resolution (e.g. 1920, 1080)
-   x_image_size = image_size[0]  
-   y_image_size = image_size[1]
+   def get_normalized_bbox(self, x_position: int, y_position: int, width: int, height: int, image_size: tuple):
+      """
+      This function will locate the bounding box of foreground (e.g. Mundo) and then
+      normalise the values from 0-1. This is important step to get YOLO format correct.
 
-   # create min/max bounding box
-   x_min = x_position
-   x_max = x_position + width
-   y_min = y_position
-   y_max = y_position + height
+      YOLO doesn't use bounding boxes where xy is the top-left corner, but xy is the 
+      center of the rectangle
+      """
+      # get background screen resolution (e.g. 1920, 1080)
+      x_image_size = image_size[0]  
+      y_image_size = image_size[1]
 
-   # convert minx/max to center co-ordinate bounding box
-   x = int(x_min + width / 2)
-   y = int(y_min + height / 2)
+      # create min/max bounding box
+      x_min = x_position
+      x_max = x_position + width
+      y_min = y_position
+      y_max = y_position + height
 
-   # normalise 
-   x /= x_image_size
-   y /= y_image_size
-   width /= x_image_size
-   height /= y_image_size
+      self.x_mins.append(x_min) 
+      self.y_mins.append(y_min)
+      self.x_maxes.append(x_max)
+      self.y_maxes.append(y_max)
 
-   normalized_bbox = (x, y, width, height)
+      # convert minx/max to center co-ordinate bounding box
+      x = int(x_min + width / 2)
+      y = int(y_min + height / 2)
 
-   return normalized_bbox#torch.tensor(normalized_bbox)
+      # normalise 
+      x /= x_image_size
+      y /= y_image_size
+      width /= x_image_size
+      height /= y_image_size
 
-def get_random_position(background_size: tuple):
-   """
-   WIP
-   """
-   x_min_bound, x_max_bound = 200, background_size[0]-400
-   y_min_bound, y_max_bound = 170, background_size[1]-250
-   
-   random_x = random.randint(x_min_bound, x_max_bound)
-   random_y = random.randint(y_min_bound, y_max_bound)
+      normalized_bbox = (x, y, width, height)
 
-   return random_x, random_y
+      return normalized_bbox#torch.tensor(normalized_bbox)
 
-def merge_background_foreground(background: Image, foreground: list, resize_mult=0, occurances=1):
-   """
-   This function will use a move_position tuple to move and paste a foreground object 
-   to the background and can resize the foreground object using a resize multiplier.
-   It returns the image and the foreground's bounding box.
-   """
-   
-   # get x, y of foreground and background
-   foreground_size = foreground.size
-   background_size = background.size
+   def get_random_position(self, background_size: tuple):
+      """
+      WIP
+      """
+      # x_min_bound, x_max_bound = 200, background_size[0]-400
+      # y_min_bound, y_max_bound = 170, background_size[1]-250
 
-   # get position of where the foreground should move to
-   x_position, y_position = get_random_position(background_size)
+      x_min_bound, x_max_bound = 500, 1300
+      y_min_bound, y_max_bound = 280, 930
+      
+      random_x = random.randint(x_min_bound, x_max_bound)
+      random_y = random.randint(y_min_bound, y_max_bound)
 
-   # get new x, y of foreground after resizing 
-   new_size_x, new_size_y = int(foreground_size[0] * resize_mult), int(foreground_size[1] * resize_mult)
-   
-   # apply resize onto foreground image
-   foreground_resized = foreground.resize((new_size_x, new_size_y))
+      return random_x, random_y
 
-   # paste resized foreground onto background at poisiton
-   background.paste(
-      foreground_resized, 
-      (x_position, y_position), 
-      foreground_resized)
-   
-   # get width and height of foreground image. 
-   _, _, width, height = foreground_resized.getbbox()
-   
-   # create bounding box
-   bbox = get_normalized_bbox(
-      x_position, 
-      y_position, 
-      width, 
-      height, 
-      background_size)
+   def merge_background_foreground(self, background: Image, foreground: list, resize_mult=0, count=0, bbox_list=[]):
+      """
+      This function will use a move_position tuple to move and paste a foreground object 
+      to the background and can resize the foreground object using a resize multiplier.
+      It returns the image and the foreground's bounding box.
 
-   return background, bbox
+      The function is recursive depending on the size of foreground
+      """
+
+      # get image we are going to add to background image
+      foreground_object = foreground[count]
+
+      # get x, y of foreground and background
+      foreground_size = foreground_object.size
+      background_size = background.size
+
+      # get position of where the foreground should move to
+      x_position, y_position = self.get_random_position(background_size)
+
+      # get new x, y of foreground after resizing 
+      new_size_x, new_size_y = int(foreground_size[0] * resize_mult), int(foreground_size[1] * resize_mult)
+      
+      # apply resize onto foreground image
+      foreground_resized = foreground_object.resize((new_size_x, new_size_y))
+
+      # paste resized foreground onto background at poisiton
+      background.paste(
+         foreground_resized, 
+         (x_position, y_position), 
+         foreground_resized)
+      
+      # get width and height of foreground image. 
+      _, _, width, height = foreground_resized.getbbox()
+      
+      self.widths.append(width)
+      self.heights.append(height)
+
+      # create bounding box
+      bbox = self.get_normalized_bbox(
+         x_position, 
+         y_position, 
+         width, 
+         height, 
+         background_size)
+
+      # save our bounding box
+      bbox_list.append(bbox)
+
+      # run function again if we havn't looped through all images in foreground
+      if count < len(foreground)-1:
+         # incrememnt count
+         self.merge_background_foreground(background, foreground, resize_mult=resize_mult, count=count+1, bbox_list=bbox_list)
+
+      return background, bbox_list
 
 
-def draw_bounding_box_for_testing(image: Image, bbox: tuple):
-   """
-   WIP
-   """
-   #plt.gca().add_patch(Rectangle((bbox[0], bbox[1]), bbox[2], bbox[3], linewidth=1, edgecolor="r", facecolor="none"))
-   
-   image_rgba = np.array(image)
-   image_rgba = cv2.cvtColor(image_rgba, cv2.COLOR_BGR2RGBA).copy()
+   def draw_bounding_box_for_testing(self, image: Image):
+      """
+      WIP
+      """
 
-   bbox_tensor = tf.convert_to_tensor(np.asarray(bbox + (0, )), dtype=tf.float32)
-   colour = tf.convert_to_tensor(np.asarray((255, 0, 0, 255)), dtype=tf.float32)
+      plt.imshow(image)
 
-   print(tf.rank(image_rgba), "\n\n")
-   image_with_boxes = tf.image.draw_bounding_boxes(image_rgba, bbox_tensor, colour)
-   print(image_with_boxes)
-   plt.imshow(image_with_boxes)
-   plt.show()
+      for i in range(len(self.widths)):
+         plt.gca().add_patch(Rectangle((self.x_mins[i], self.y_mins[i], self.x_maxes[i], self.y_maxes[i]), self.widths[i], self.heights[i], linewidth=1, edgecolor="r", facecolor="none"))
+
+      plt.show()
 
 
-def stretch_for_YOLO(image: Image, size=(320,320)):
-   """
-   YOLO trains on square images that are multiples of 32. e.g. 320x320, 352x352, 416x416, 608x608, etc.
-   """
-   return image.resize(size)
+   def stretch_for_YOLO(self, image: Image, size=(320,320)):
+      """
+      YOLO trains on square images that are multiples of 32. e.g. 320x320, 352x352, 416x416, 608x608, etc.
+      """
+      return image.resize(size)
 
-def save_image_with_YOLO_bb_txt(image: Image, bbox: tuple, output_path: str, object_id: int, file_name="test"):
-   """
-   WIP
-   """
-   # get file name
-   # mundo_name = mundo_path.split("/")[-1:][0].replace(".png", "")
-   content = f"{object_id} {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}"
+   def save_image_with_YOLO_bb_txt(self, image: Image, bbox: tuple, output_path: str, object_id: int, file_name="test"):
+      """
+      WIP
+      """
+      # get file name
+      # mundo_name = mundo_path.split("/")[-1:][0].replace(".png", "")
+      content = f"{object_id} {bbox[0]} {bbox[1]} {bbox[2]} {bbox[3]}"
 
-   txt_file = open(f"{output_path}/{file_name}.txt", "w")
-   txt_file.write(content)
-   txt_file.close()
+      txt_file = open(f"{output_path}/{file_name}.txt", "w")
+      txt_file.write(content)
+      txt_file.close()
 
-   image.save(f"{output_path}/{file_name}.jpg")
+      image.save(f"{output_path}/{file_name}.jpg")
 
-def load_images_and_run_all(background_path_folder: str, mundo_folder_path: str, output_path: str):
-   """
-   WIP
-   """
-   for i, file_name in enumerate(os.listdir(background_path_folder)):
-      for j, mundo_file_name in enumerate(os.listdir(mundo_folder_path)):
+   def load_images_and_run_all(self, occurances=1):
+      """
+      WIP
+      """
+      # loop through background path folder to get file names
+      for i, file_name in enumerate(os.listdir(self.background_path_folder)):
 
-         try: #DS.Store files may be captured 
-            background_image = Image.open(f"{background_path_folder}/{file_name}")
-            mundo_image = Image.open(f"{mundo_folder_path}/{mundo_file_name}")
-         except Exception as e:
-            print(e)
-         else:
+         # loop through mundo images path folder to get file names
+         for j, mundo_file_name in enumerate(os.listdir(self.mundo_path_folder)):
 
-            images_not_null = background_image is not None and mundo_image is not None
+            # get size of axe images
+            axe_sizes = len(os.listdir(axe_path_folder))-1
 
-            if images_not_null:
-               # create image
-               image, bbox = merge_background_foreground(
-                  background_image,
-                  mundo_image, 
-                  resize_mult=0.30,
-                  occurances=2)
-               
-               save_image_with_YOLO_bb_txt(image, bbox, output_path, 1, file_name=f"final_{i}_{j}")
+            # since there are less images of axe we use mod to keep looping through the axe images
+            axe_file_name = f"axe_{j % axe_sizes}.png"
+
+            try: #DS.Store files may be captured 
+               background_image = Image.open(f"{self.background_path_folder}/{file_name}")
+               mundo_image = Image.open(f"{self.mundo_path_folder}/{mundo_file_name}")
+               axe_image = Image.open(f"{self.axe_path_folder}/{axe_file_name}")
+
+            except Exception as e:
+               print(e)
+
+            else:
+               images_not_null = background_image is not None and mundo_image is not None
+
+               # since we are using .paste() method, the background image will be the final image
+               # image is a clearer variable here
+               image = background_image
+
+               if images_not_null:
+                  # e.g. if there is a mundo and axe images we want to paste, 
+                  # if occurances is 2, there will be 2 mundos and 2 axes 
+                  for _ in range(occurances):
+                     # create image
+                     image, bboxes = self.merge_background_foreground(
+                        image,
+                        [mundo_image, axe_image], 
+                        resize_mult=0.30)
+
+                  print(bboxes, "\n", len(bboxes))
+                     
+                  self.draw_bounding_box_for_testing(image)
+
+                  #save_image_with_YOLO_bb_txt(image, bbox, output_path, 1, file_name=f"final_{i}_{j}")
 
 if __name__ == '__main__':
-   # example
-   #background_path = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/baron_pit_frames/frame_0.png"
    background_path_folder = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/baron_pit_frames/"
-   #mundo_path = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/output_parsed_frames/mundo_500.png"
    mundo_path_folder = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/output_parsed_frames/mundo/"
-   # output_path_annotations = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/merged_images/annotations/"
-   # output_path_images = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/merged_images/images/"
+   axe_path_folder = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/output_parsed_frames/axe/"
    output_path = "/Users/alpharaoh/Documents HDD/Machine Learning/Machine-Learning/Projects/MDAI/dataset/output/merged_images/"
 
-   # background_image = Image.open(background_path)
-   # mundo_image = Image.open(mundo_path)
-
-   # image, bbox = merge_background_foreground(
-   #    background_image,
-   #    mundo_image, 
-   #    resize_mult=0.34, 
-   #    move_position=(600, 100))
-
    #save_image_with_YOLO_bb_txt(stretch_for_YOLO(image), bbox, output_path, 1)
+   create = CreateDataset(background_path_folder, mundo_path_folder, axe_path_folder, output_path)
 
-   load_images_and_run_all(background_path_folder, mundo_path_folder, output_path)
+   create.load_images_and_run_all(occurances=2)
