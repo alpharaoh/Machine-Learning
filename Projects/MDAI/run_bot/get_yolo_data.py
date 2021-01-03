1d02e130a5acd8c6b368a93e4fa3e76441eea2c9
alpharaoh <alpharaohh@gmail.com>
2021-01-03 16:12:52 +0000

    adding templates folder for Flask server

--- Projects/MDAI/run_bot/get_yolo_data.py
+++ Projects/MDAI/run_bot/get_yolo_data.py
@@ -1,3 +1,11 @@
+"""
+This program will grab the users screen and pass the frame to YOLO for it to be able
+to infer where the mundo(s) or axe(s) are. 
+
+Todo: 
+
+- Give frame data to YOLO and get bounding boxes of objects
+"""
 # python3 detect.py --weights /Users/alpharaoh/Downloads/best\ \(1\).pt --img 416 --conf 0.6 --source ./../dataset/output/test_yolo/full_game/
 from mss import mss
 import cv2
@@ -5,8 +13,7 @@ import numpy as np
 from PIL import Image
 import sys
 
-sys.path.append('../yolov5/')
-#import detect
+import flask_server
 
 print("Loaded.\n")
 
@@ -26,8 +33,8 @@ class YoloData():
          "height": y_res, 
       }
  
-   def pass_frame_to_YOLO(self, frame):
-      detect.detect(save_img=False)
+   def frame_to_server(self, frame):
+      flask_server.video_feed(frame)
 
    def get_frame(self, sct):
       frame = sct.grab(self.settings)
@@ -43,14 +50,15 @@ class YoloData():
        with mss() as sct:
         while True:
             frame = self.get_frame(sct) #numpy array
+            self.frame_to_server(frame)
 
-            if self.debug:
-               cv2.imshow('Test', np.array(frame)) #output screen, for testing only
+            # if self.debug:
+            #    cv2.imshow('Test', np.array(frame)) #output screen, for testing only
 
-               if cv2.waitKey(25) & 0xFF == ord('q'): #Press Q on debug windows to exit
-                  cv2.destroyAllWindows()
-                  break
+            #    if cv2.waitKey(25) & 0xFF == ord('q'): #Press Q on debug windows to exit
+            #       cv2.destroyAllWindows()
+            #       break
 
 if __name__ == '__main__':
-   yolo = YoloData(1920, 1080, monitor_number=0, debug=True)
+   yolo = YoloData(1920, 1080, monitor_number=1, debug=True)
    yolo.run()
